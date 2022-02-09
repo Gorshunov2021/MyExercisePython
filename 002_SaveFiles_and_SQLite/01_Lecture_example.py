152281c12f931ac6eb963f072ab038b7871f1cca
Gorshunov2021 <gorshunov2.20@gmail.com>
2022-02-09 22:29:34 +0300

    except2

--- 002_SaveFiles_and_SQLite/01_Lecture_example.py
+++ 002_SaveFiles_and_SQLite/01_Lecture_example.py
@@ -1,27 +1,27 @@
-# n = input("Введите целое число: ")
-# try:
-#     n = int(n)
-#     print("Удачно")
-# except:
-#     print("Что-то пошло не так")
-#
-# try:
-#    n = input('Введите целое число: ')
-#    n = int(n)
-#    print("Все нормально. Вы ввели число", n)
-# except ValueError:
-#    print("Вы ввели не целое число")
+n = input("Введите целое число: ")
+try:
+    n = int(n)
+    print("Удачно")
+except:
+    print("Что-то пошло не так")
+
+try:
+   n = input('Введите целое число: ')
+   n = int(n)
+   print("Все нормально. Вы ввели число", n)
+except ValueError:
+   print("Вы ввели не целое число")
 
 
-# try:
-#     a = float(input("Введите делимое: "))
-#     b = float(input("Введите делитель: "))
-#     c = a / b
-#     print("Частное: ", c)
-# except ValueError:
-#     print("Нельзя вводить строки")
-# except ZeroDivisionError:
-#     print("Нельзя делить на ноль")
+try:
+    a = float(input("Введите делимое: "))
+    b = float(input("Введите делитель: "))
+    c = a / b
+    print("Частное: ", c)
+except ValueError:
+    print("Нельзя вводить строки")
+except ZeroDivisionError:
+    print("Нельзя делить на ноль")
 
 
 try:
