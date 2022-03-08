4002c68579b10f2a2714ce53bfce396d9fbac9d8
Gorshunov2021 <gorshunov2.20@gmail.com>
2022-03-08 15:35:24 +0300

    commit 8.03.2022 Unittestbook3

---
+++ 007_Unittest/03_unittest_book.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+class BookTest(unittest.TestCase):
+    def test_name(self):
+        self.assertEqual(("История Доколумбовых цивилизаций"), "История Доколумбовых цивилизаций")
+
+    def test_author(self):
+        self.assertEqual(("Мануэль Галич"), "Мануэль Галич")
+
+    def test_year(self):
+        self.assertEqual(("2008"), "2010")
+
+if __name__ == '__main__':
+    unittest.main()
